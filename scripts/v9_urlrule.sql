1070a1dfae753160bc7bb6dc08d824abbe7f433b
ayang502 <ymf.watt@163.com>
2015-06-28 00:25:44 +0800

    v9_urlrule.sql

--- scripts/v9_urlrule.sql
+++ scripts/v9_urlrule.sql
@@ -30,7 +30,7 @@ CREATE TABLE `v9_urlrule` (
   `urlrule` varchar(255) NOT NULL,
   `example` varchar(255) NOT NULL,
   PRIMARY KEY (`urlruleid`)
-) ENGINE=MyISAM AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `v9_urlrule` (
 
 LOCK TABLES `v9_urlrule` WRITE;
 /*!40000 ALTER TABLE `v9_urlrule` DISABLE KEYS */;
-INSERT INTO `v9_urlrule` VALUES (1,'content','category',1,'{$categorydir}{$catdir}/index.html|{$categorydir}{$catdir}/{$page}.html','news/china/1000.html'),(6,'content','category',0,'index.php?m=content&c=index&a=lists&catid={$catid}|index.php?m=content&c=index&a=lists&catid={$catid}&page={$page}','index.php?m=content&c=index&a=lists&catid=1&page=1'),(11,'content','show',1,'{$year}/{$catdir}_{$month}{$day}/{$id}.html|{$year}/{$catdir}_{$month}{$day}/{$id}_{$page}.html','2010/catdir_0720/1_2.html'),(12,'content','show',1,'{$categorydir}{$catdir}/{$year}/{$month}{$day}/{$id}.html|{$categorydir}{$catdir}/{$year}/{$month}{$day}/{$id}_{$page}.html','it/product/2010/0720/1_2.html'),(16,'content','show',0,'index.php?m=content&c=index&a=show&catid={$catid}&id={$id}|index.php?m=content&c=index&a=show&catid={$catid}&id={$id}&page={$page}','index.php?m=content&c=index&a=show&catid=1&id=1'),(17,'content','show',0,'show-{$catid}-{$id}-{$page}.html','show-1-2-1.html'),(18,'content','show',0,'content-{$catid}-{$id}-{$page}.html','content-1-2-1.html'),(30,'content','show',0,'list-{$catid}-{$page}.html','list-1-1.html'),(31,'content','show',1,'{$id}.html','1000.html'),(32,'content','show',1,'{$year}/{$month}/{$day}/{$id}.html','2015/05/24/1000.html'),(33,'content','show',1,'sitemap_{$id}.xml','sitemap_1000.xml '),(34,'content','show',1,'@strtotime({$year}-{$month}-{$day})d{$id}.html','{TimeStamp}d{ContentID}.html'),(35,'content','category',1,'{$categorydir}/index.html','index.html');
+INSERT INTO `v9_urlrule` VALUES (1,'content','category',1,'{$categorydir}{$catdir}/index.html|{$categorydir}{$catdir}/{$page}.html','news/china/1000.html'),(6,'content','category',0,'index.php?m=content&c=index&a=lists&catid={$catid}|index.php?m=content&c=index&a=lists&catid={$catid}&page={$page}','index.php?m=content&c=index&a=lists&catid=1&page=1'),(11,'content','show',1,'{$year}/{$catdir}_{$month}{$day}/{$id}.html|{$year}/{$catdir}_{$month}{$day}/{$id}_{$page}.html','2010/catdir_0720/1_2.html'),(12,'content','show',1,'{$categorydir}{$catdir}/{$year}/{$month}{$day}/{$id}.html|{$categorydir}{$catdir}/{$year}/{$month}{$day}/{$id}_{$page}.html','it/product/2010/0720/1_2.html'),(16,'content','show',1,'index.php?m=content&c=index&a=show&catid={$catid}&id={$id}|index.php?m=content&c=index&a=show&catid={$catid}&id={$id}&page={$page}','index.php?m=content&c=index&a=show&catid=1&id=1'),(17,'content','show',1,'show-{$catid}-{$id}-{$page}.html','show-1-2-1.html'),(18,'content','show',1,'content-{$catid}-{$id}-{$page}.html','content-1-2-1.html'),(30,'content','category',1,'list-{$catid}-{$page}.html','list-1-1.html'),(31,'content','show',1,'{$E_Name_s}.html','stanford.html'),(32,'content','show',1,'{$id}.html','1000.html'),(33,'content','show',0,'[@pubSchool(\'{$SchoolID}\')]{$id}.html','11.html'),(34,'content','show',0,'{$TimeStamp}d{$id}.html','136251273d1.html');
 /*!40000 ALTER TABLE `v9_urlrule` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-24 22:41:44
+-- Dump completed on 2015-06-28  0:25:27
